f18ad323c461147471136032b94eacc454fc43fb
Carl Scarlett <carl.orion.scarlett@gmail.com>
2021-06-15 06:29:49 +0800

    Created a zodcredit tag-plugin to use in place of a photo credit hack

---
+++ scripts/zodcredit.js
@@ -0,0 +1,31 @@
+hexo.extend.tag.register('zodcredit', function(args, content) {
+
+  var backgroundColourClass = args[0] || 'black';
+
+  var typeClass = "fas fa-camera";
+  // switch (args[1] || 'photo') {
+  //   // case 'photo':
+  //   //   typeClass = 'fas fa-camera'
+  //   //   break;
+  // }
+
+  var urlText = args[2] || 'https://redperegrine.net';
+
+  var bodyText = hexo.render.renderSync({
+    text: content,
+    engine: 'markdown'
+  }).slice(3, -5);  // slice removes leading <p> and trailing </p>
+
+  var output =
+  `
+  <a style="background-color:${backgroundColourClass};color:white;text-decoration:none;padding:4px 6px;font-size:12px;line-height:1.2;display:inline-block;border-radius:3px" href="${urlText}">
+    <i class="${typeClass} mr-2"></i>${bodyText}
+  </a>
+  `
+
+  return output;
+
+}, {
+  async: true,
+  ends: true
+});
